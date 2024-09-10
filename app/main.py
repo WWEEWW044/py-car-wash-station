be96093838c018a39765040f37a6c603240a9833
Andrey Hordiienko <return.ginger@gmail.com>
2024-09-10 22:08:18 +0300

    fixed type annotation

--- app/main.py
+++ app/main.py
@@ -25,14 +25,14 @@ class CarWashStation:
         self.average_rating = (total_rating + rate) / self.count_of_ratings
         self.average_rating = round(self.average_rating, 1)
 
-    def calculate_washing_price(self, car: "Car") -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         comfort_class = car.comfort_class
         clean_diff = self.clean_power - car.clean_mark
         rating = self.average_rating
         distance = self.distance_from_city_center
         return (comfort_class * clean_diff * rating) / distance
 
-    def wash_single_car(self, cars: list) -> list:
+    def wash_single_car(self, cars: list[Car]) -> list[Car]:
         washed_cars = []
         for car in cars:
             if isinstance(car, Car) and self.clean_power >= car.clean_mark:
